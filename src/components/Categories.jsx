497b9c4a9c8b8e038a0357c48b433b080f8a2e96
Carlos Eduardo Ortiz <carlosklinton.2016@gmail.com>
2023-09-28 12:55:12 -0500

    se arreglo el problema de la visibilidad del emcabezado de filtros

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -5,21 +5,26 @@ export const Categories = ({categorie, deleteJob, deleteFilter}) => {
   return (
     
     <div className='filtros-container'>
-            <div className='filtro'>
+           {
+              categorie.length > 0 && 
+              ( 
+                <div className='filtro'>
                 
-                {categorie.map((ele, index) => (
-                    <div className='categoria' key={index}>
-                        <h1 >{ele}</h1>
-                    
-                        <img className='delete-button' src={deleteButtom} onClick={() => deleteJob(ele)} alt="" />
-                    </div>
-                ))}
+                      {categorie.map((ele, index) => (
+                          <div className='categoria' key={index}>
+                              <h1 >{ele}</h1>
+                          
+                              <img className='delete-button' src={deleteButtom} onClick={() => deleteJob(ele)} alt="" />
+                          </div>
+                      ))}
 
-                {
-                    categorie.length > 0 ? <button className='clear' onClick={deleteFilter}>clear</button>:''
-                }
+                      {
+                          categorie.length > 0 ? <button className='clear' onClick={deleteFilter}>clear</button>:''
+                      }
 
-            </div>
+                </div>
+              )
+           }
    
     </div>
     
--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 
-
 export const Jobs = ({jobs, jobSelect}) => {
   return (
 
